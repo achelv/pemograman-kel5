0350d1459ba2f7568afa7789271df06531d09de0
napichan <dwikiilman01@gmail.com>
2026-03-31 22:39:43 +0700

    fix update navianProfile

--- src/pages/ShelyProfile.jsx
+++ src/pages/ShelyProfile.jsx
@@ -11,13 +11,13 @@ function ShelyProfile() {
         className="w-48 h-48 rounded-full mb-6 shadow-lg"
       />
       <h1 className="text-4xl font-bold mb-2">Shely Rahmatika</h1>
-      <p className="text-gray-700 mb-2">Kelas 12 RPL | Domisili: [Isi Domisili]</p>
+      <p className="text-gray-700 mb-2">Mahasiswi Teknologi Informasi | kelas T2E | Domisili: Ponorogo</p>
       <p className="max-w-xl text-center mt-4 text-gray-800">
         Halo! Saya Shely Rahmatika. Saya menyukai teknologi, web design, dan React JS.
         Saya senang membuat desain yang simpel tapi menarik. Saya juga aktif belajar Tailwind CSS
         dan membuat project landing page dan profil tim. Saya selalu berusaha berkembang,
         belajar hal baru, dan bekerja sama dengan tim. Hobi saya termasuk membaca, coding, dan
-        mencoba hal kreatif yang menantang. (Ini contoh deskripsi, bisa diisi minimal 100 kata)
+        mencoba hal kreatif yang menantang.
       </p>
     </div>
   );
--- src/pages/navianProfile.jsx
+++ src/pages/navianProfile.jsx
@@ -11,13 +11,15 @@ function NavianProfile() {
         className="w-48 h-48 rounded-full mb-6 shadow-lg"
       />
       <h1 className="text-4xl font-bold mb-2">Dwiki Ilman Nafian</h1>
-      <p className="text-gray-700 mb-2">Kelas 12 RPL | Domisili: [Isi Domisili]</p>
+      <p className="text-gray-700 mb-2">Mahasiswi Teknologi Informasi | Kelas T2E | Domisili: Ponorogo</p>
       <p className="max-w-xl text-center mt-4 text-gray-800">
-        Halo! Saya Dwiki Ilman Nafian. Saya menyukai teknologi, web design, dan React JS.
-        Saya senang membuat desain yang simpel tapi menarik. Saya juga aktif belajar Tailwind CSS
-        dan membuat project landing page dan profil tim. Saya selalu berusaha berkembang,
-        belajar hal baru, dan bekerja sama dengan tim. Hobi saya termasuk membaca, coding, dan
-        mencoba hal kreatif yang menantang. (Ini contoh deskripsi, bisa diisi minimal 100 kata)
+        Halo! Saya Dwiki Ilman Nafian. Saya memiliki semangat belajar tinggi,
+        disiplin,dan bertanggung jawab dalam setiap pekerjaan yang dilakukan.
+        Saya terbiasa bekerja secara mandiri maupun dalam tim, 
+        serta mampu beradaptasi dengan lingkungan baru. Saya memiliki ketertarikan pada pengembangan diri,
+        teknologi,dan kreativitas,sehingga selalu berusaha meningkatkan kemampuan yang dimiliki.
+        Dengan komunikasi yang baik dan sikap yang positif,saya siap menghadapi tantangan serta memberikan kontribusi terbaik.
+        Saya percaya bahwa konsistensi,kerja keras,dan kemauan untuk terus belajar adalah kunci utama untuk mencapai tujuan dan berkembang menjadi pribadi yang lebih baik setiap harinya.
       </p>
     </div>
   );
