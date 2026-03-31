47af455190d0a14b3d77e416e5b0f6d2eb3b64e7
achelv <shelyrahma1395@gmail.com>
2026-03-31 21:29:07 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/shely-rahmatika" element={<ShelyProfile />} />
+        <Route path="/shely-profile" element={<ShelyProfile />} />
         <Route path="/navi-profile" element={<ShelyProfile />} />
       </Routes>
     </Router>
--- src/pages/ShelyProfile.jsx
+++ src/pages/ShelyProfile.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import TeamCard from "../components/TeamCard";
-import shely from "../src/assets/ijazah.jpg";
+import shely from "../assets/ijazah.jpg";
 
 function ShelyProfile() {
   return (
     <div className="min-h-screen bg-green-100 flex flex-col items-center justify-center p-6">
       <img
-        src="/assets/ijazah.jpg"
-        alt="Shely"
-        className="w-48 h-48 rounded-full mb-6 shadow-lg"
+         src={shely}
+         alt="Shely"
+         className="w-48 h-48 rounded-full mb-6 shadow-lg"
       />
       <h1 className="text-4xl font-bold mb-2">Shely Rahmatika</h1>
       <p className="text-gray-700 mb-2">Kelas 12 RPL | Domisili: [Isi Domisili]</p>
