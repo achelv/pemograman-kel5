2e8dde1182b6f68773e1f0cba2779248f21d002d
achelv <shelyrahma1395@gmail.com>
2026-03-31 19:16:29 +0700

    menambahkan foto shelyProfile

--- src/pages/ShelyProfile.jsx
+++ src/pages/ShelyProfile.jsx
@@ -4,7 +4,7 @@ function ShelyProfile() {
   return (
     <div className="min-h-screen bg-green-100 flex flex-col items-center justify-center p-6">
       <img
-        src="/assets/shely.jpg"
+        src="/assets/ijasah.jpg"
         alt="Shely"
         className="w-48 h-48 rounded-full mb-6 shadow-lg"
       />
