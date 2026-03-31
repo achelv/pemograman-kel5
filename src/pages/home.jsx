48fb2d4be188fd6719729200aeddcdfa7b861946
napichan <dwikiilman01@gmail.com>
2026-04-01 00:02:08 +0700

    update home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import TeamCard from "../components/TeamCard";
+import logo from "../assets/logo.jpg"; // ⬅️ tambahin ini
 
 function Home() {
   const team = [
-    { name: "Shely Rahmatika", route: "/shely-profile", image: "src/assets/ijazah.jpg" },
+    { name: "Shely Rahmatika Devi", route: "/shely-profile", image: "src/assets/ijazah.jpg" },
     { name: "Shely Rahmatika", route: "/shely-rahmatika", image: "/assets/shely.jpg" },
     { name: "Dwiki Ilman Navian", route: "/navi-profile", image: "src/assets/dwi.jpeg" },
   ];
-  
 
   return (
     <div className="min-h-screen bg-green-50 flex flex-col items-center justify-center p-6">
       <h1 className="text-5xl font-extrabold mb-10">Profil Tim</h1>
+      {/* LOGO */}
+      <img 
+        src={logo} 
+        alt="logo" 
+        className="w-24 mb-4"
+      />
+
+      <h1 className="text-5xl font-extrabold mb-10">Anggota Kelompok 5</h1>
+
       <div className="flex gap-6">
         {team.map((member) => (
           <TeamCard
